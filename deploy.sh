854c76b34ff41eff0dfc23c2eba5891c4215bc12
nboutin <boutwork@gmail.com>
2018-11-16 22:40:08 +0100

    Add liquidpromptrc

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,19 @@
 #! /bin/sh
 
-rm ~/.gitconfig
-ln -s $PWD/git/.gitconfig ~
+# Git
+file='.gitconfig'
+rm ~/${file}
+ln -s $PWD/git/${file} ~
+echo "deploy ${file}"
+
+# Ubuntu
+file='.bash_aliases'
+rm ~/${file}
+ln -s $PWD/ubuntu/${file} ~
+echo "deploy ${file}"
+
+# Liquidprompt
+file='liquidpromptrc'
+rm ~/.config/${file}
+ln -s $PWD/liquidprompt/${file} ~/.config
+echo "deploy ${file}"
